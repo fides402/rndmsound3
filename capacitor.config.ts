6a4a08bf8be11493c672b4dbd092df3a8c91aa1e
fides402 <135014190+fides402@users.noreply.github.com>
2026-02-24 15:51:04 +0100

    Fix formatting in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.fides402.rndmsound3',
-    appName: 'Random Sound',
-    webDir: 'dist',
-    server: {
-        androidScheme: 'https'
-    }
+  appId: 'com.fides402.rndmsound3',
+  appName: 'Random Sound',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
